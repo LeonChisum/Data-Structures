bfa98f648277668fe7186170343391b9e50ff893
LeonChisum <leonchisum3306@gmail.com>
2020-08-23 20:49:06 -0400

    finished Readme questions for Stack

--- stack/stack.py
+++ stack/stack.py
@@ -11,10 +11,45 @@ return elements in Last In First Out order.
    implementing a Stack?
 """
 # Stack class using array
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+#
+#     def __len__(self):
+#         # checking to see if the stack is empty
+#         if len(self.storage) == 0:
+#             print("stack is empty!")
+#             return self.size
+#         else:
+#             print(len(self.storage))
+#             return len(self.storage)
+#
+#     def push(self, value):
+#         if value is None:
+#             print("There is nothing to add to the stack!")
+#         else:
+#             self.storage.append(value)
+#             self.size + 1
+#             return len(self.storage)
+#
+#     def pop(self):
+#         # checking to see if the stack is empty
+#         if len(self.storage) == 0:
+#             print("stack is empty!")
+#         else:
+#             return self.storage.pop()
+
+# Stack class using linked lists
+class Node:
+    def __init__(self,value, next=None):
+        self.value = value
+        self.next = None
+
+
 class Stack:
     def __init__(self):
-        self.size = 0
-        self.storage = []
+        self.head == None
 
     def __len__(self):
         # checking to see if the stack is empty
@@ -34,7 +69,8 @@ class Stack:
             return len(self.storage)
 
     def pop(self):
-        # checking to see if the stack is empty
+        # checking to se
+        # e if the stack is empty
         if len(self.storage) == 0:
             print("stack is empty!")
         else:
