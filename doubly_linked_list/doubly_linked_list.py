1b215829c6b78122c21f44d2055d8629f07c750e
LeonChisum <leonchisum3306@gmail.com>
2020-08-27 20:34:06 -0400

    doubly_linked_list.py delete, remove_head , and remove_tail test passed

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -27,16 +27,18 @@ class DoublyLinkedList:
     the old head node's previous pointer accordingly.
     """
     def add_to_head(self, value):
-        if self.head is None:
-            return None
+        new_node = ListNode(value)
+        self.length += 1
+
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node
         else:
             cur = self.head
-            new_node = ListNode(value)
             old_node = cur
             self.head = new_node
             new_node.next = old_node
             old_node.prev = new_node
-            self.length += 1
 
     """
     Removes the List's current head node, making the
@@ -44,8 +46,11 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_head(self):
-        pass
-            
+        if self.head is None:
+            return None
+        head_value = self.head.value
+        self.delete(self.head)
+        return head_value
     """
     Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
@@ -60,7 +65,11 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_tail(self):
-        pass
+        if self.tail is None:
+            return None
+        tail_value = self.tail.value
+        self.delete(self.tail)
+        return tail_value
             
     """
     Removes the input node from its current spot in the 
@@ -81,7 +90,30 @@ class DoublyLinkedList:
     order of the other elements of the List.
     """
     def delete(self, node):
-        pass
+    # If list is empty
+        if self.head is None and self.tail is None:
+            return
+    # the list only has one node
+        self.length -= 1
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+
+    # the node to be deleted is the head
+        if self.head == node:
+            self.head = node.next
+            node.next.prev = None
+
+    # the node to be deleted is the tail
+        if self.tail == node:
+            self.tail = node.prev
+            node.prev.next = None
+    # the node is in between head and tail
+        else:
+            if node.prev:
+                node.prev.next = node.next
+            if node.next:
+                node.next.prev = node.prev
 
     """
     Finds and returns the maximum value of all the nodes 
